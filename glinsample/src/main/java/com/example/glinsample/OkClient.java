0d7754f22633b1bc425aa20071259519d6b6f4ad
qibin <qibin@smart-haier.com>
2016-09-12 11:45:53 +0800

    up to date

--- glinsample/src/main/java/com/example/glinsample/OkClient.java
+++ glinsample/src/main/java/com/example/glinsample/OkClient.java
@@ -1,4 +1,4 @@
-package com.example.glinsample;
+package com.haiersmart.sfnation.net;
 
 import android.os.Handler;
 import android.os.Looper;
@@ -9,15 +9,28 @@ import org.loader.glin.Callback;
 import org.loader.glin.Params;
 import org.loader.glin.Result;
 import org.loader.glin.client.IClient;
+import org.loader.glin.factory.ParserFactory;
 import org.loader.glin.helper.Helper;
-import org.loader.glin.parser.*;
+import org.loader.glin.interceptor.IResultInterceptor;
+import org.loader.glin.parser.Parser;
 
 import java.io.File;
 import java.io.IOException;
+import java.security.SecureRandom;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 import okhttp3.Call;
 import okhttp3.Headers;
@@ -38,60 +51,90 @@ public class OkClient implements IClient {
     private OkHttpClient mClient;
     private Handler mHandler;
     private ParserFactory mParserFactory;
+    private IResultInterceptor mResultInterceptor;
+
     private long mTimeOut = DEFAULT_TIME_OUT;
     private boolean isDebug;
 
     public OkClient() {
-        mClient = new OkHttpClient();
+        mClient = buildClient();
+    }
+
+    private OkHttpClient buildClient() {
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        try {
+            SSLContext sc = SSLContext.getInstance("SSL");
+            sc.init(null, new TrustManager[]{new X509TrustManager() {
+                @Override
+                public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+                }
+
+                @Override
+                public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+                }
+
+                @Override
+                public X509Certificate[] getAcceptedIssuers() {
+                    X509Certificate[] x509Certificates = new X509Certificate[0];
+                    return x509Certificates;
+                }
+            }}, new SecureRandom());
+
+            builder.sslSocketFactory(sc.getSocketFactory());
+            builder.hostnameVerifier(new HostnameVerifier() {
+                @Override
+                public boolean verify(String hostname, SSLSession session) {
+                    return true;
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return builder.build();
     }
 
     @Override
-    public <T> void get(String url, Object tag, Callback<T> callback) {
-        final Request request = new Request.Builder()
-                .url(url).build();
-        call(request, callback, tag, new StringBuilder());
+    public <T> void get(String url, final LinkedHashMap<String, String> header, Object tag, Callback<T> callback) {
+        final Request request = new Request.Builder().url(url).build();
+        call(request,header, callback, tag, new StringBuilder());
     }
 
     @Override
-    public <T> void post(String url, Params params, Object tag, Callback<T> callback) {
+    public <T> void post(String url, final LinkedHashMap<String, String> header, Params params, Object tag, Callback<T> callback) {
         StringBuilder debugInfo = new StringBuilder();
         MultipartBody builder = createRequestBody(params, debugInfo);
-        Request request = new Request.Builder()
-                .url(url).post(builder).build();
-        call(request, callback, tag, debugInfo);
+        Request request = new Request.Builder().url(url).post(builder).build();
+        call(request,header, callback, tag, debugInfo);
     }
 
     @Override
-    public <T> void post(String url, String json, Object tag, final Callback<T> callback) {
+    public <T> void post(String url, final LinkedHashMap<String, String> header, String json, Object tag, final Callback<T> callback) {
         StringBuilder debugInfo = new StringBuilder();
-        Request request = new Request.Builder().url(url)
-                .post(createJsonBody(json, debugInfo)).build();
-        call(request, callback, tag, debugInfo);
+        Request request = new Request.Builder().url(url).post(createJsonBody(json, debugInfo)).build();
+        call(request,header, callback, tag, debugInfo);
     }
 
     @Override
-    public <T> void put(String url, Params params, Object tag, Callback<T> callback) {
+    public <T> void put(String url, final LinkedHashMap<String, String> header, Params params, Object tag, Callback<T> callback) {
         StringBuilder debugInfo = new StringBuilder();
         MultipartBody builder = createRequestBody(params, debugInfo);
-        Request request = new Request.Builder()
-                .url(url).put(builder).build();
-        call(request, callback, tag, debugInfo);
+        Request request = new Request.Builder().url(url).put(builder).build();
+        call(request,header, callback, tag, debugInfo);
     }
 
     @Override
-    public <T> void put(String url, String json, Object tag, Callback<T> callback) {
+    public <T> void put(String url, final LinkedHashMap<String, String> header, String json, Object tag, Callback<T> callback) {
         StringBuilder debugInfo = new StringBuilder();
-        Request request = new Request.Builder()
-                .url(url).put(createJsonBody(json, debugInfo)).build();
-        call(request, callback, tag, debugInfo);
+        Request request = new Request.Builder().url(url).put(createJsonBody(json, debugInfo)).build();
+        call(request,header, callback, tag, debugInfo);
     }
 
     @Override
-    public <T> void delete(String url, Object tag, Callback<T> callback) {
+    public <T> void delete(String url, final LinkedHashMap<String, String> header, Object tag, Callback<T> callback) {
         StringBuilder debugInfo = new StringBuilder();
-        final Request request = new Request.Builder()
-                .url(url).delete().build();
-        call(request, callback, tag, debugInfo);
+        final Request request = new Request.Builder().url(url).delete().build();
+        call(request,header, callback, tag, debugInfo);
     }
 
     @Override
@@ -122,7 +165,6 @@ public class OkClient implements IClient {
     @Override
     public void timeout(long ms) {
         mTimeOut = ms;
-
     }
 
     @Override
@@ -133,64 +175,64 @@ public class OkClient implements IClient {
     private OkHttpClient cloneClient() {
         return mClient.newBuilder()
                 .connectTimeout(mTimeOut, TimeUnit.MILLISECONDS)
-                .readTimeout(mTimeOut, TimeUnit.MILLISECONDS)
-                .writeTimeout(mTimeOut, TimeUnit.MILLISECONDS)
+//                .readTimeout(mTimeOut, TimeUnit.MILLISECONDS)
+//                .writeTimeout(mTimeOut, TimeUnit.MILLISECONDS)
                 .build();
     }
 
-    private <T> void call(Request request, final Callback<T> callback,
+    private <T> void call(Request request, final LinkedHashMap<String, String> header,
+                          final Callback<T> callback,
                           final Object tag, StringBuilder debugInfo) {
         String info = debugInfo.toString();
         debugInfo.delete(0, debugInfo.length());
 
-        debugInfo.append("URL->" + request.url().toString() + "\n");
-        debugInfo.append("Method->" + request.method() + "\n");
-        debugInfo.append("Header->");
+        debugInfo.append("URL->").append(request.url().toString()).append("\n");
+        debugInfo.append("Method->").append(request.method()).append("\n");
 
+        LinkedHashMap<String, String> map = (header != null && !header.isEmpty())
+                ? header : headers();
         Request.Builder builder = request.newBuilder();
         builder.tag(tag);
-        LinkedHashMap<String, String> map = headers();
-        if(map != null && !map.isEmpty()) {
-            for(Iterator<String> iterator = map.keySet().iterator(); iterator.hasNext();) {
+        if (map != null && !map.isEmpty()) {
+            for (Iterator<String> iterator = map.keySet().iterator(); iterator.hasNext();) {
                 String key = iterator.next();
                 String value = map.get(key);
                 if(value == null) continue;
-                debugInfo.append(key + ":" + value + ";");
                 builder.addHeader(key, value);
             }
-            request = builder.build();
         }
-        debugInfo.append("\n");
-
+        request = builder.build();
         final Call call = cloneClient().newCall(request);
         call.enqueue(new okhttp3.Callback() {
             @Override
             public void onFailure(Call call, IOException e) {
                 prntInfo("Error->" + e.getMessage());
-                Result<T> result = new Result<T>();
+                Result<T> result = new Result<>();
                 result.ok(false);
                 result.setMessage(e.getMessage());
-                callback(callback, result);
+                callback(call, callback, result);
             }
 
             @Override
             public void onResponse(final Call call, Response response) throws IOException {
                 String resp = response.body().string();
-                prntInfo("Response->" + resp);
-                callback(callback, (Result<T>) getParser(callback.getClass()).parse(callback.getClass(), resp));
+                prntInfo("Response->" + replaceBlank(resp));
+                Result<T> res = (Result<T>) getParser(callback.getClass()).parse(callback.getClass(), resp);
+                callback(call, callback, res);
             }
         });
 
-        String header = request.headers().toString();
-        if(!TextUtils.isEmpty(header)) {
-            debugInfo.append("Header->" + header + "\n");
+        String debugHeader = request.headers().toString();
+        if(!TextUtils.isEmpty(debugHeader)) {
+            debugInfo.append("Header->").append(debugHeader).append("\n");
         }
+        debugInfo.append("\n");
 
         debugInfo.append(info);
         prntInfo(debugInfo.toString());
     }
 
-    private <T> org.loader.glin.parser.Parser getParser(Class<T> klass) {
+    private <T> Parser getParser(Class<T> klass) {
         Class<?> type = Helper.getType(klass);
         if (type.isAssignableFrom(List.class)) {
             return mParserFactory.getListParser();
@@ -198,24 +240,48 @@ public class OkClient implements IClient {
         return mParserFactory.getParser();
     }
 
+    private static String replaceBlank(String str) {
+        String dest = str;
+        if (dest != null) {
+            Pattern p = Pattern.compile("\\s*|\t|\r|\n");
+            Matcher m = p.matcher(str);
+            dest = m.replaceAll("");
+        }
+        return dest;
+    }
+
     private void prntInfo(String info) {
         if (!isDebug) { return;}
+
         Log.d("Glin", "*******************--BEGIN--*******************");
         Log.d("Glin", info);
         Log.d("Glin", "********************--END--********************");
     }
 
-    private <T> void callback(final Callback<T> callback, final Result<T> result) {
+    private <T> void callback(final Call call, final Callback<T> callback, final Result<T> result) {
         if(Looper.myLooper() == Looper.getMainLooper()) {
-            callback.onResponse(result);
+            realCallback(call, callback, result);
         }else {
             getHandler().post(new Runnable() {
                 @Override
                 public void run() {
-                    callback.onResponse(result);
+                    realCallback(call, callback, result);
                 }
             });
         }
+    }
+
+    private <T> void realCallback(Call call, Callback<T> callback, Result<T> result) {
+//        if (isDebug) { Log.d("Glin", "call is canceled ? " + call.isCanceled());}
+        if (call.isCanceled()) { return;}
+        if (!intercept(result)) { callback.onResponse(result);}
+    }
+
+    private <T> boolean intercept(final Result<T> result) {
+        if (mResultInterceptor != null && mResultInterceptor.intercept(result)) {
+            return true;
+        }
+        return false;
     }
 
     private MultipartBody createRequestBody(Params params, StringBuilder debugInfo) {
@@ -228,7 +294,7 @@ public class OkClient implements IClient {
             String key = iterator.next();
             String value = map.get(key);
             if(value == null) continue;
-            debugInfo.append(key + ":" + value + ";");
+            debugInfo.append(key).append(":").append(value).append(";");
             builder.addPart(Headers.of("Content-Disposition",
                     "form-data; name=\""+ key +"\""),
                     RequestBody.create(null, value));
@@ -241,7 +307,7 @@ public class OkClient implements IClient {
             String key = iterator.next();
             File file = files.get(key);
             if(file == null) continue;
-            debugInfo.append(key + ":" + file.getName());
+            debugInfo.append(key).append(":").append(file.getName());
             builder.addPart(Headers.of("Content-Disposition",
                     "form-data; name=\"" + key + "\";filename=\""+ file.getName() +"\""),
                     RequestBody.create(MediaType.parse("application/octet-stream"), file));
@@ -252,14 +318,18 @@ public class OkClient implements IClient {
     }
 
     private RequestBody createJsonBody(String json, StringBuilder debugInfo) {
-        debugInfo.append("RequestJson->" + json);
-        RequestBody body = RequestBody.create(MediaType
-                .parse("application/json;charset=utf-8"), json);
+        debugInfo.append("RequestJson->").append(json);
+        RequestBody body = RequestBody.create(MediaType.parse("application/json;charset=utf-8"), json);
         return body;
     }
 
     private Handler getHandler() {
         if(mHandler == null) { mHandler = new Handler(Looper.getMainLooper());}
         return mHandler;
+    }
+
+    @Override
+    public void resultInterceptor(IResultInterceptor interceptor) {
+        mResultInterceptor = interceptor;
     }
 }
