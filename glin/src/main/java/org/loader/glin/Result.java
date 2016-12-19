443767ec5a99d99f7e979c24e712f7a2f6b22462
qibin <qibin@smart-haier.com>
2016-12-19 16:45:50 +0800

    add Result.code

--- glin/src/main/java/org/loader/glin/Result.java
+++ glin/src/main/java/org/loader/glin/Result.java
@@ -52,11 +52,6 @@ public class Result<T> {
         this.obj = obj;
     }
 
-    /**
-     * @deprecated use {@link #getCode()} instead
-     * @param <V>
-     * @return
-     */
     public <V> V assertGetObj() {
         return (V) getObj();
     }
